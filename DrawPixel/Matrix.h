b8c0bc578864996e7b4837c19a4d0d4d39452667
LucianoRubioTaracido <luciano-rubio@hotmail.com>
2019-08-07 07:55:34 -0500

    added .gitignore

---
+++ DrawPixel/Matrix.h
@@ -0,0 +1,40 @@
+#pragma once
+#ifndef MATRIX_H
+#define MATRIX_H
+
+#include <vector>
+#include "Vector.h"
+#include <iostream>
+
+
+class Matrix
+{
+private:
+	std::vector <std::vector<float>> mat;
+	unsigned rows;
+	unsigned cols;
+	unsigned tempRows;
+	unsigned tempCols;
+
+public:
+	Matrix(const Matrix& other);
+	Matrix(unsigned r, unsigned c);
+
+	void ModMatrix(int value, int rows, int cols);
+	//Matrix(Matrix&);
+
+	//Matrix
+	Matrix& operator= (Matrix);
+	Matrix operator+ (Matrix&);
+	Matrix operator- (Matrix&);
+	Matrix operator* (Matrix&);
+	//Matrix operator* (Matrix&);
+
+	Matrix transpose();
+
+	float& operator() (const unsigned&, const unsigned&);
+
+	virtual ~Matrix();
+};
+
+#endif // !MATRIX_H
